626e1a2c6ef25a2ec3c4b919d3b964b70fceea15
Анна Кугай <112816144+kuhai-anna@users.noreply.github.com>
2024-06-18 05:33:22 +0300

    refactor: move some of the logic outside the try...catch block

--- test.js
+++ test.js
@@ -4,60 +4,56 @@ const sortStatisticsByCountDescending = require("./helpers/sortStatsByCountDesce
 const getTotalLetterNumber = require("./helpers/getTotalLetterNumber");
 const getPercentageValues = require("./helpers/getPercentageValues");
 
+const letterCounts = [];
+
+const countLetter = (char) => {
+  const isLetter = /[a-zA-Z]/.test(char);
+
+  if (!isLetter) {
+    return;
+  }
+
+  const lowercaseLetter = char.toLowerCase();
+
+  // check the presence of a letter in the array
+  const existingLetter = letterCounts.find(
+    ({ letter }) => letter === lowercaseLetter
+  );
+
+  if (existingLetter) {
+    existingLetter.count += 1;
+  } else {
+    letterCounts.push({ letter: lowercaseLetter, count: 1 });
+  }
+};
+
 try {
   const data = fs.readFileSync(
     "/Users/annakuhai/Documents/test-ai/bigText.txt",
     "utf8"
   );
 
-  const letterCounts = [];
-
-  const countLetter = (char) => {
-    const isLetter = /[a-zA-Z]/.test(char);
-
-    if (!isLetter) {
-      return;
-    }
-
-    const lowercaseLetter = char.toLowerCase();
-
-    // check the presence of a letter in the array
-    const existingLetter = letterCounts.find(
-      ({ letter }) => letter === lowercaseLetter
-    );
-
-    if (existingLetter) {
-      existingLetter.count += 1;
-    } else {
-      letterCounts.push({ letter: lowercaseLetter, count: 1 });
-    }
-  };
-
   for (let char of data) {
     countLetter(char);
   }
-
-  const totalLetterNumber = getTotalLetterNumber(letterCounts);
-
-  const statisticsInDescendingOrder =
-    sortStatisticsByCountDescending(letterCounts);
-
-  console.log("Total number of letters", getTotalLetterNumber(letterCounts));
-
-  console.log(
-    "In alphabetical order",
-    sortStatisticsAlphabetically(letterCounts)
-  );
-
-  console.log(
-    "In descending order",
-    sortStatisticsByCountDescending(letterCounts)
-  );
-
-  console.log(
-    "In percentage value",
-    getPercentageValues(statisticsInDescendingOrder, totalLetterNumber)
-  );
 } catch (err) {
   console.error(err);
 }
+
+const totalLetterNumber = getTotalLetterNumber(letterCounts);
+const statisticsInDescendingOrder =
+  sortStatisticsByCountDescending(letterCounts);
+
+console.log("Total number of letters", getTotalLetterNumber(letterCounts));
+console.log(
+  "In alphabetical order",
+  sortStatisticsAlphabetically(letterCounts)
+);
+console.log(
+  "In descending order",
+  sortStatisticsByCountDescending(letterCounts)
+);
+console.log(
+  "In percentage value",
+  getPercentageValues(statisticsInDescendingOrder, totalLetterNumber)
+);
